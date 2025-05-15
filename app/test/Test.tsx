d0dfa790051ab467a0987a4940aa574b0717b784
dieschlag <dischlermateo@outlook.fr>
2025-05-15 23:47:49 +0700

    added input logic to detect and time the wwitiing of a sentence

--- app/test/Test.tsx
+++ app/test/Test.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { getRandomQuote } from "@/lib/quote";
 import { Quote } from "@/types/quote";
 import { useEffect, useState } from "react";
+import { TestInput } from "./TestInput";
 
 export function TestClient() {
   const [randomQuote, setRandomQuote] = useState<Quote | undefined>(undefined);
@@ -11,7 +12,7 @@ export function TestClient() {
   const [errorMessage, setErrorMessage] = useState<string | undefined>(
     undefined
   );
-
+  const [isTestFinished, setIsTestFinished] = useState<boolean>(false);
   useEffect(() => {
     getRandomQuote().then((response) => {
       if ("data" in response) {
@@ -20,23 +21,28 @@ export function TestClient() {
         setErrorMessage(response.message!);
       }
     });
-  });
+  }, []);
+
   return (
-    <div className="flex flex-col gap-8 items-center">
+    <div className="flex flex-col w-full gap-8 items-center">
       <p>
         Click on "Start Test" to generate a random text and test your writing
         speed!
       </p>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col w-full items-center">
         {testStarted ? (
-          <div className="flex flex-col">
-            {!errorMessage && randomQuote ? (
+          !errorMessage && randomQuote ? (
+            <div className="flex flex-col w-full items-center">
               <p>{randomQuote.paragraph}</p>
-            ) : (
-              <p>Loading...</p>
-            )}
-          </div>
+              <TestInput
+                quote={randomQuote.paragraph}
+                setIsTestFinished={setIsTestFinished}
+              />
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )
         ) : (
           <Button
             className="px-4 py-2 bg-slate-600 text-white font-bold w-fit"
---
+++ app/test/TestInput.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useRef, useState } from "react";
+import { ChangeEvent } from "react";
+
+type TestInputProps = {
+  quote: string;
+  setIsTestFinished: (value: boolean) => void;
+};
+
+export function TestInput({ quote, setIsTestFinished }: TestInputProps) {
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const startTimeRef = useRef<number | null>(null);
+  const rafRef = useRef<number | null>(null);
+  const runningRef = useRef(false); // ← la vraie source de vérité
+  const [elapsed, setElapsed] = useState<number>(0);
+  function verifyWord(e: ChangeEvent<HTMLInputElement>) {
+    if (!runningRef.current && e.currentTarget.value.length > 0) {
+      runningRef.current = true;
+      startTimeRef.current = performance.now();
+      tick();
+    }
+
+    const input = e.currentTarget.value;
+    const textLength = input.length;
+
+    if (textLength + currentIndex <= quote.length) {
+      console.log(
+        "Potential futur element to display",
+        quote.slice(currentIndex, currentIndex + textLength)
+      );
+      if (quote.slice(currentIndex, currentIndex + textLength) === input) {
+        setCurrentIndex(currentIndex + input.length);
+        e.currentTarget.value = "";
+        console.log("to display as valid: ", quote.slice(0, currentIndex));
+      }
+    }
+  }
+
+  const tick = () => {
+    if (!runningRef.current) return;
+    if (startTimeRef.current === null) {
+      startTimeRef.current = performance.now();
+      tick();
+    } else {
+      rafRef.current = requestAnimationFrame(() => {
+        if (startTimeRef.current !== null) {
+          const now = performance.now();
+          setElapsed(now - startTimeRef.current);
+          tick(); // boucle
+        }
+      });
+    }
+  };
+
+  const stopChrono = () => {
+    runningRef.current = false;
+    if (rafRef.current !== null) {
+      cancelAnimationFrame(rafRef.current);
+      rafRef.current = null;
+    }
+  };
+
+  useEffect(() => {
+    if (currentIndex === quote.length) {
+      stopChrono();
+    }
+  }, [currentIndex]);
+
+  useEffect(() => {
+    console.log("elapsed", elapsed);
+  }, [elapsed]);
+
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-row items-center border border-gray-300 rounded-md px-2">
+        <p className="text-base text-green-400 font-bold">
+          {quote.slice(0, currentIndex)}
+        </p>
+        <input
+          type="text"
+          onChange={(e) => verifyWord(e)}
+          className="bg-transparent w-60 text-black font-mono text-base py-2 focus:outline-none"
+        />
+      </div>
+      <div className="flex flex-row">
+        <span>Time spent: </span>
+        <span>{elapsed}</span>
+      </div>
+    </div>
+  );
+}
